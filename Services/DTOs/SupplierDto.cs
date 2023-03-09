f2e4d69bfa25748af109f54b98c5bd1459d93a90
Manan Patel <manan.patel4812@gmail.com>
2023-03-09 16:16:16 +0530

    DTOs and Factories

--- Services/DTOs/SupplierDto.cs
+++ Services/DTOs/SupplierDto.cs
@@ -4,13 +4,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Services.DTOs
-{
-    public class SupplierDto
-    {
-        public SupplierDto(int id, string name, string alias, string email, string contactNo, bool active)
-        {
+namespace Services.DTOs;
 
-        }
+public class SupplierDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Alias { get; set; }
+    public string Email { get; set; }
+    public string ContactNo { get; set; }
+    public bool Active { get; set; }
+
+    public IEnumerable<FacilityDto> Facilities;
+    public IEnumerable<ContactDto> Contacts;
+
+    public SupplierDto(int id, string name, string alias, string email, string contactNo, bool active, IEnumerable<FacilityDto> facilities, IEnumerable<ContactDto> contacts)
+    {
+        Id = id;
+        Name = name;
+        Alias = alias;
+        Email = email;
+        ContactNo = contactNo;
+        Active = active;
+        Facilities = facilities;
+        Contacts = contacts;
     }
-}
+};
--- Services/Factories/ReportingPeriodFactory.cs
+++ Services/Factories/ReportingPeriodFactory.cs
@@ -1,12 +1,21 @@
-﻿using System;
+﻿using BusinessLogic.ReferenceLookups;
+using BusinessLogic.ReportingPeriodRoot.DomainModels;
+
+using Services.Factories.Interface;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Services.Factories;
 
-public class ReportingPeriodFactory
+public class ReportingPeriodFactory : IReportingPeriodFactory
 {
-
+    //public ReportingPeriod CreateNewReportingperiod(string CollectionTimePeriod, ReportingPeriodType reportingPeriodType, ReportingPeriodStatus reportingPeriodStatus, DateTime startDate, DateTime endDate, bool active)
+    //{
+    //    var ReportingPeriod = new ReportingPeriod(CollectionTimePeriod, reportingPeriodType, reportingPeriodStatus, startDate, endDate, active);
+    //    return ReportingPeriod;
+    //}
 }
